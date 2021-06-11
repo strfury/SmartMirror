5eebaa87c4d353de1217ff4a9c34d76ef7dfe21e
Gregory Presser <gpress2222@gmail.com>
2021-06-11 17:15:31 -0400

    Added more documentation

--- smart_mirror/src/App.js
+++ smart_mirror/src/App.js
@@ -1,5 +1,6 @@
 import MainPage from "./Components/MainPage"; 
 
+// this is the first component that will render and calls the MainPage Component
 const App = ()=> (<MainPage />);
 
 export default App;
